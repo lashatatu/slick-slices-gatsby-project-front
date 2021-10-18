3347290063f8829f934f67f0ebcb757d6aab470e
lasha tatulashvili <lashatatu@gmail.com>
2021-10-19 03:11:15 +0400

    Initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function fourOhFourPage(){
+  return (
+    <div>
+      Hey - 404 not exist
+    </div>
+  )
+}
---
+++ src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function sliceMastersPage() {
+  return (
+    <div>
+      <p>sliceMastersPage</p>
+    </div>
+  );
+}
