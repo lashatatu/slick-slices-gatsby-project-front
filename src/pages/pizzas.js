3e64d4a26da273f451d88a3ae8468c1b8e23db4a
lasha tatulashvili <lashatatu@gmail.com>
2021-10-20 03:12:51 +0400

    18

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,10 +1,38 @@
 import React from "react";
-import Layout from '../components/Layout';
+import { graphql } from "gatsby";
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
-    <div>
-      <p>PizzasPage</p>
-    </div>
+    <>
+      <PizzaList pizzas={pizzas}/>
+    </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
