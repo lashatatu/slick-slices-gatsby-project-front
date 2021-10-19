55f37026d6357b1ae5c5a4356bca4b70093a1600
lasha tatulashvili <lashatatu@gmail.com>
2021-10-19 06:58:46 +0400

    9

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,69 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from "styled-components";
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  .logo{
+    transform: translateY(-25%);
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    text-align: center;
+    list-style: none;
+  }
+
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+
+    &:hover {
+      color: red;
+    }
+  }
+`;
 
 const Nav = () => {
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to={"/"}>Hot now</Link>
         </li>
         <li>
-          <Link to={"/pizzas"}>Pizzas</Link>
+          <Link to={"/pizzas"}>Pizza Menu</Link>
         </li>
         <li>
-          <Link to={"/"}>Logo</Link>
+          <Link to={"/"}><Logo/></Link>
         </li>
         <li>
           <Link to={"/slicemasters"}>SliceMasters</Link>
@@ -21,7 +72,7 @@ const Nav = () => {
           <Link to={"/order"}>Order Ahead!</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 };
 
