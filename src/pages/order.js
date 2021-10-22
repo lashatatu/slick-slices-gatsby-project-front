2cab4b20369b35cb9496fcd96c705e024c0d349b
lasha tatulashvili <lashatatu@gmail.com>
2021-10-23 03:52:31 +0400

    35

--- src/pages/order.js
+++ src/pages/order.js
@@ -5,6 +5,8 @@ import { graphql } from "gatsby";
 import Img from 'gatsby-image';
 import calculatePizzaPrice from '../utils/calculatePizzaPrice';
 import formatMoney from '../utils/formatMoney';
+import OrderStyles from '../styles/OrderStyles';
+import MenuItemStyles from '../styles/MenuItemStyles';
 
 export default function OrderPage({ data }) {
   const { values, updateValue } = useForm({ name: "", email: "" });
@@ -12,7 +14,7 @@ export default function OrderPage({ data }) {
   return (
     <>
       <SEO title={"Order a Pizza"} />
-      <form>
+      <OrderStyles>
         <fieldset>
           <legend>Your Info</legend>
           <label htmlFor="name">Name</label>
@@ -32,10 +34,10 @@ export default function OrderPage({ data }) {
             onChange={updateValue}
           />
         </fieldset>
-        <fieldset>
+        <fieldset className={'menu'}>
           <legend>Menu</legend>
           {pizzas.map((pizza) => (
-            <div key={pizza.id}>
+            <MenuItemStyles key={pizza.id}>
               <Img width={'50'} height={'50'} fluid={pizza.image.asset.fluid} alt={pizza.name}/>
               <div>
                 <h2>{pizza.name}</h2>
@@ -47,13 +49,13 @@ export default function OrderPage({ data }) {
                   </button>
                 ))}
               </div>
-            </div>
+            </MenuItemStyles>
           ))}
         </fieldset>
-        <fieldset>
+        <fieldset className={'order'}>
           <legend>Order</legend>
         </fieldset>
-      </form>
+      </OrderStyles>
     </>
   );
 }
