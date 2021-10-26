12871dedad0f81b98e4df94dfcd657c949663524
lasha tatulashvili <lashatatu@gmail.com>
2021-10-26 13:40:25 +0400

    fix

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -102,14 +102,14 @@ export default function Logo() {
   return (
     <LogoStyles className="logo">
       <div className="inner">
-        <span className="est">EST 1988</span>
+        <span className="est">EST 1994</span>
         <h1>
           <span className="slicks">
             <span className="letter S">L</span>
-            <span className="letter l">A</span>
-            <span className="letter i">S</span>
-            <span className="letter c">H</span>
-            <span className="letter k">A</span>
+            <span className="letter l">a</span>
+            <span className="letter i">s</span>
+            <span className="letter c">h</span>
+            <span className="letter k">a</span>
             <span className="letter apos">'</span>
             <span className="letter s">s</span>
           </span>
