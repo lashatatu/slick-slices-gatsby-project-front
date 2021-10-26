efd784cb40971d0d20b2025e78f085b57e1be4c4
lasha tatulashvili <lashatatu@gmail.com>
2021-10-26 14:03:22 +0400

    SEO added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import useLatestData from "../utils/useLatestData";
-import { HomePageGrid } from '../styles/Grids';
-import LoadingGrid from '../components/LoadingGrid';
-import ItemGrid from '../components/ItemGrid';
+import { HomePageGrid } from "../styles/Grids";
+import LoadingGrid from "../components/LoadingGrid";
+import ItemGrid from "../components/ItemGrid";
+import SEO from '../components/SEO';
 
-function CurrentlySlicing({slicemasters}) {
+function CurrentlySlicing({ slicemasters }) {
   return (
     <div>
       <h2 className="center">
@@ -19,6 +20,7 @@ function CurrentlySlicing({slicemasters}) {
     </div>
   );
 }
+
 function HotSlices({ hotSlices }) {
   return (
     <div>
@@ -26,7 +28,7 @@ function HotSlices({ hotSlices }) {
         <span className="mark tilt">Hot Slices!</span>
       </h2>
       <p>Come on by, buy the slice!</p>
-      {!hotSlices && <LoadingGrid count={4} />}{' '}
+      {!hotSlices && <LoadingGrid count={4} />}{" "}
       {hotSlices && !hotSlices?.length && <p>Nothin' in the Case</p>}
       {hotSlices?.length && <ItemGrid items={hotSlices} />}
     </div>
@@ -38,6 +40,8 @@ export default function HomePage() {
 
   return (
     <div className="center">
+      <SEO/>
+
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
       <HomePageGrid>
